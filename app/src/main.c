240e727f3c5b94b6d736b9885c30f0a5f2fb66d0
Tirth Patel <tirthpatel199561@gmail.com>
2026-08-29 12:39:39 -0400

    * K_yield information

--- app/src/main.c
+++ app/src/main.c
@@ -10,7 +10,7 @@ LOG_MODULE_REGISTER(l1_task1, LOG_LEVEL_DBG);
 #define PRIO_LOW    7
 #define PRIO_MED    5
 #define PRIO_HIGH   3
-#define PRIO_COOP   -1
+#define PRIO_COOP   -3
 
 void t_low_fn(void *p1, void *p2, void *p3)
 {
@@ -39,19 +39,30 @@ void t_high_fn(void *p1, void *p2, void *p3)
 void t_coop_fn(void *p1, void *p2, void *p3)
 {
     // while (1) {
-        for (int i = 0; i < 5 ; i ++)
+        int i;
+        for (i = 0; i < 5 ; i ++)
         {
             LOG_DBG("T_Coop_iteration %d",i);
+            
         }
-        // LOG_DBG("T_Coop_iteration");
-        k_yield();
-    // }
+        LOG_DBG("T_Coop_iteration resets");
+        i = 0;
+        // k_yield();
+    // } 
+
+/* 
+    Here because we haven't used the continous while loop,
+    the threads terminates after yielding.
+
+    If we are using the while it would be essential to use k_msleep(), 
+    if not the preemptible threads will never execute.
+*/
     
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, t_low_fn,NULL, NULL, NULL, PRIO_LOW, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, t_med_fn,NULL, NULL, NULL, PRIO_MED, 0, 0);
-K_THREAD_DEFINE(thread_c, STACK_SIZE, t_high_fn,NULL, NULL, NULL, PRIO_MED, 0, 0);           
+// K_THREAD_DEFINE(thread_a, STACK_SIZE, t_low_fn,NULL, NULL, NULL, PRIO_LOW, 0, 0);
+// K_THREAD_DEFINE(thread_b, STACK_SIZE, t_med_fn,NULL, NULL, NULL, PRIO_MED, 0, 0);
+// K_THREAD_DEFINE(thread_c, STACK_SIZE, t_high_fn,NULL, NULL, NULL, PRIO_MED, 0, 0);           
 K_THREAD_DEFINE(thread_coop, STACK_SIZE, t_coop_fn,NULL,NULL, NULL, PRIO_COOP, 0, 0);           
 
 
