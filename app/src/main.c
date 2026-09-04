5507663714b6b8a66fae457c690e36051569a6a9
NEP-ENG\tirth.patel <tirth.patel@neptronic.com>
2026-09-04 10:23:19 -0400

    changes

--- app/src/main.c
+++ app/src/main.c
@@ -8,7 +8,7 @@ LOG_MODULE_REGISTER(l2_assignment, LOG_LEVEL_DBG);
 
 
 #define STACK_SIZE 1024
-#define INCREMENT    7
+#define INCREMENT    10
 #define PRIO   3
 
 static volatile uint32_t cntr = 0;
@@ -22,10 +22,10 @@ void incr_cntr(void *p1, void *p2, void *p3)
     ARG_UNUSED(p3);
 
     while (cntr < INCREMENT) {
-        LOG_INF("[ %s ] at [ %d ]", k_thread_name_get(k_current_get()), k_uptime_get_32());
-        LOG_DBG("Incrementing the counter ");
+        LOG_INF("[ %s ] Incrementing at [ %lld ]", k_thread_name_get(k_current_get()), k_uptime_get());
         cntr ++;
-        k_msleep(300);;
+        LOG_INF("Counter at %d",cntr);
+        k_msleep(30);
     }
 }
 
@@ -36,7 +36,6 @@ K_THREAD_DEFINE(thread_b, STACK_SIZE, incr_cntr,NULL,NULL, NULL, PRIO, 0, 0);
 int main(void)
 {
     LOG_INF("START test running over...........");
-    k_msleep(300);
     return 0;
 }
 
