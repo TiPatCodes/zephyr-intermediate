cc537612f742dc68b91014bfac93c8647bb4f6a1
NEP-ENG\tirth.patel <tirth.patel@neptronic.com>
2026-09-04 12:40:36 -0400

    changeds

--- app/src/main.c
+++ app/src/main.c
@@ -5,26 +5,26 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE      1024
 #define PRIO            5
-#define INCREMENTS      5000   /* each thread increments this many times */
+#define INCREMENTS      10000   /* each thread increments this many times */
 
 /* Shared state - intentionally unprotected */
 static volatile uint32_t counter;
 
 static struct k_sem done_sem;
 
-static K_MUTEX_DEFINE(counter_mutex);
+// static K_MUTEX_DEFINE(counter_mutex);
 
 void worker_fn(void *p1, void *p2, void *p3)
 {
     const char *name = k_thread_name_get(k_current_get());
 
     for (int i = 0; i < INCREMENTS; i++) {
-        k_mutex_lock(&counter_mutex, K_FOREVER);
+        // k_mutex_lock(&counter_mutex, K_FOREVER);
         counter++;
-        k_mutex_unlock(&counter_mutex);
+        // k_mutex_unlock(&counter_mutex);
     }
 
-    LOG_INF("[%s] finished", name);
+    LOG_INF("[%s] finished, Execution time: %lld ms", name, k_uptime_get());
     k_sem_give(&done_sem);
 }
 
@@ -42,11 +42,11 @@ int main(void)
 
     LOG_INF("=== L2 Demo 2: Mutex Protection ===");
     LOG_INF("Expected final value: %d", INCREMENTS * 2);
-
+    
     /* Wait for both workers to complete */
     k_sem_take(&done_sem, K_FOREVER);
     k_sem_take(&done_sem, K_FOREVER);
-
+    
     LOG_INF("Actual  final value: %u", counter);
 
     if (counter == INCREMENTS * 2) {
