4ff1e741feedbc7efc061532cb9b333d0b11f4e1
Tirth Patel <tirthpatel199561@gmail.com>
2026-09-03 22:34:28 -0400

    l2 assignment

--- app/src/main.c
+++ app/src/main.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdint.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
@@ -6,69 +7,29 @@ LOG_MODULE_REGISTER(l1_task1, LOG_LEVEL_DBG);
 
 
 #define STACK_SIZE 1024
+#define INCREMENT    7
+#define PRIO   3
 
-#define PRIO_LOW    7
-#define PRIO_MED    5
-#define PRIO_HIGH   3
-#define PRIO_COOP   -3
+static uint32_t cntr = 0;
 
-void t_low_fn(void *p1, void *p2, void *p3)
+void incr_cntr(void *p1, void *p2, void *p3)
 {
     while (1) {
-        k_msleep(300);
-        LOG_DBG("T_LOW running");
+        // k_msleep(300);
+        LOG_DBG("running");
     }
 }
 
-void t_med_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        k_msleep(200);
-        LOG_DBG("T_MED running");
-    }
-}
+K_MUTEX_DEFINE(mutex_1);
 
-void t_high_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        k_msleep(100);
-        LOG_DBG("T_HIGH running");
-    }
-}
-
-void t_coop_fn(void *p1, void *p2, void *p3)
-{
-    // while (1) {
-        int i;
-        for (i = 0; i < 5 ; i ++)
-        {
-            LOG_DBG("T_Coop_iteration %d",i);
-            
-        }
-        LOG_DBG("T_Coop_iteration resets");
-        i = 0;
-        // k_yield();
-    // } 
-
-/* 
-    Here because we haven't used the continous while loop,
-    the threads terminates after yielding.
-
-    If we are using the while it would be essential to use k_msleep(), 
-    if not the preemptible threads will never execute.
-*/
-    
-}
-
-// K_THREAD_DEFINE(thread_a, STACK_SIZE, t_low_fn,NULL, NULL, NULL, PRIO_LOW, 0, 0);
-// K_THREAD_DEFINE(thread_b, STACK_SIZE, t_med_fn,NULL, NULL, NULL, PRIO_MED, 0, 0);
-// K_THREAD_DEFINE(thread_c, STACK_SIZE, t_high_fn,NULL, NULL, NULL, PRIO_MED, 0, 0);           
-K_THREAD_DEFINE(thread_coop, STACK_SIZE, t_coop_fn,NULL,NULL, NULL, PRIO_COOP, 0, 0);           
+K_THREAD_DEFINE(thread_a, STACK_SIZE, incr_cntr,NULL, NULL, NULL, PRIO, 0, 0);
+K_THREAD_DEFINE(thread_b, STACK_SIZE, incr_cntr,NULL,NULL, NULL, PRIO, 0, 0);           
 
 
 int main(void)
 {
-    LOG_INF("Total running over...........");
+    LOG_INF("START test running over...........");
+    k_msleep(300);
     return 0;
 }
 
